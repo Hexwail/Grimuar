fe5475c0a6c3c13cb306ff59f7ef84ca3db80782
Hexwail <ivankuzmichov2008@gmail.com>
2025-06-02 18:43:19 +0300

    Update MagicSystem.java

--- src/main/java/com/hexwail/grimuar/MagicSystem.java
+++ src/main/java/com/hexwail/grimuar/MagicSystem.java
@@ -1,1 +1,16 @@
+package com.hexwail.grimuar.magic;
 
+public abstract class Rune {
+    private final String name;
+
+    public Rune(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Метод активації руни, який треба реалізувати в конкретних рунах
+    public abstract void activate();
+}
