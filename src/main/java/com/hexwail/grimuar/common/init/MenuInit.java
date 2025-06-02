3fc762d7ee9593edca95d1d981a6ae45dbe8199e
Hexwail <ivankuzmichov2008@gmail.com>
2025-06-02 19:17:49 +0300

    Update MenuInit.java

--- src/main/java/com/hexwail/grimuar/common/init/MenuInit.java
+++ src/main/java/com/hexwail/grimuar/common/init/MenuInit.java
@@ -1,22 +1,19 @@
-package com.hexwail.grimuar.common.init;
+package com.hexwail.grimuar.init;
 
-import com.hexwail.grimuar.GrimuarMod;
-import com.hexwail.grimuar.common.menu.SpellEditorMenu;
+import com.hexwail.grimuar.gui.SpellEditorMenu;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod.EventBusSubscriber(modid = GrimuarMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MenuInit {
-    public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(ForgeRegistries.MENU_TYPES, GrimuarMod.MODID);
+    public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(ForgeRegistries.MENU_TYPES, "grimuar");
 
     public static final RegistryObject<MenuType<SpellEditorMenu>> SPELL_EDITOR_MENU =
-            MENUS.register("spell_editor_menu", () -> new MenuType<>(SpellEditorMenu::new));
+        MENUS.register("spell_editor_menu", () -> IForgeMenuType.create(SpellEditorMenu::new));
 
     public static void register() {
-        MENUS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        MENUS.register();
     }
 }
