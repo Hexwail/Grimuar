96c7cbbda79c90a26cbddcb7722743c618b6c5f4
Hexwail <ivankuzmichov2008@gmail.com>
2025-06-02 19:08:10 +0300

    Create SpellEditorMenu.java

---
+++ src/main/java/com/hexwail/grimuar/common/menu/SpellEditorMenu.java
@@ -0,0 +1,41 @@
+package com.hexwail.grimuar.common.menu;
+
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ObjectHolder;
+
+public class SpellEditorMenu extends AbstractContainerMenu {
+    
+    @ObjectHolder("grimuar:spell_editor_menu")
+    public static MenuType<SpellEditorMenu> TYPE;
+
+    public SpellEditorMenu(int id, Inventory playerInventory) {
+        super(TYPE, id);
+        
+        // Тут можна буде додати слоти для рун/заклять
+        // А поки просто заповнення інвентаря гравця
+        for (int y = 0; y < 3; ++y) {
+            for (int x = 0; x < 9; ++x) {
+                this.addSlot(new Slot(playerInventory, x + y * 9 + 9, 8 + x * 18, 84 + y * 18));
+            }
+        }
+
+        for (int x = 0; x < 9; ++x) {
+            this.addSlot(new Slot(playerInventory, x, 8 + x * 18, 142));
+        }
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return true;
+    }
+
+    @Override
+    public ItemStack quickMoveStack(Player player, int index) {
+        return ItemStack.EMPTY;
+    }
+}
