e9d3a2105d5cd0076000ac157951012afc62e657
Hexwail <ivankuzmichov2008@gmail.com>
2025-06-02 19:02:13 +0300

    Create SpellEditorScreen.java

---
+++ src/main/java/com/hexwail/grimuar/client/screen/SpellEditorScreen.java
@@ -0,0 +1,38 @@
+package com.hexwail.grimuar.client.screen;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+import com.hexwail.grimuar.common.menu.SpellEditorMenu;
+
+public class SpellEditorScreen extends AbstractContainerScreen<SpellEditorMenu> {
+    private static final ResourceLocation TEXTURE = new ResourceLocation("grimuar", "textures/gui/spell_editor.png");
+
+    public SpellEditorScreen(SpellEditorMenu menu, Inventory inv, Component title) {
+        super(menu, inv, title);
+        this.imageWidth = 176;
+        this.imageHeight = 166;
+    }
+
+    @Override
+    protected void renderBg(GuiGraphics graphics, float partialTicks, int mouseX, int mouseY) {
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        graphics.blit(TEXTURE, leftPos, topPos, 0, 0, imageWidth, imageHeight);
+    }
+
+    @Override
+    protected void renderLabels(GuiGraphics graphics, int mouseX, int mouseY) {
+        graphics.drawString(this.font, this.title, 8, 6, 0x404040, false);
+    }
+
+    @Override
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
+        this.renderBackground(graphics);
+        super.render(graphics, mouseX, mouseY, partialTicks);
+        this.renderTooltip(graphics, mouseX, mouseY);
+    }
+}
