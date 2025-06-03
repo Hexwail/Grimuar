e6afd7618d7c8db25b12d86e732ccf98f1dc1404
Hexwail <ivankuzmichov2008@gmail.com>
2025-06-03 15:00:13 +0300

    Update RunePanel.java

--- src/main/java/com/hexwail/grimuar/client/gui/editor/RunePanel.java
+++ src/main/java/com/hexwail/grimuar/client/gui/editor/RunePanel.java
@@ -1,53 +1,70 @@
 package com.hexwail.grimuar.client.gui.editor;
 
+import com.hexwail.grimuar.client.data.RuneTabManager;
+import com.hexwail.grimuar.client.data.RuneTab;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.network.chat.Component;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.components.Widget;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RunePanel {
 
-    private List<RuneTab> tabs = new ArrayList<>();
-    private RuneTab currentTab;
+    private final List<RuneTab> runeTabs = new ArrayList<>();
+    private int selectedTab = 0;
+    private final List<Widget> widgets = new ArrayList<>();
+    private boolean inEditMode = false;
+    private RuneTab editingTab = null;
 
     public RunePanel() {
-        initTabs();
+        RuneTabManager.load();
+        runeTabs.addAll(RuneTabManager.getTabs());
     }
 
-    private void initTabs() {
-        // Створюємо вкладку "Всі", яка містить усі відомі руни
-        RuneTab allRunesTab = new RuneTab("Всі");
-        // Тут треба додати всі руни до allRunesTab
-        allRunesTab.addRunes(loadAllKnownRunes());
-
-        tabs.add(allRunesTab);
-        currentTab = allRunesTab;
-    }
-
-    private List<Rune> loadAllKnownRunes() {
-        // Псевдо-код: заміни на свій метод завантаження всіх рун
-        List<Rune> allRunes = new ArrayList<>();
-        // allRunes.add(...);
-        // ...
-        return allRunes;
-    }
-
-    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
-        // Відмалювати вкладки
-        for (RuneTab tab : tabs) {
-            tab.renderTabButton(graphics);
+    public void render(GuiGraphics graphics, int x, int y, int width, int height) {
+        // Рендер заголовків вкладок
+        for (int i = 0; i < runeTabs.size(); i++) {
+            RuneTab tab = runeTabs.get(i);
+            graphics.drawString(graphics.getFont(), tab.getName(), x + i * 60, y, i == selectedTab ? 0xFFFFFF : 0xAAAAAA);
         }
 
-        // Відмалювати вміст поточної вкладки
-        if (currentTab != null) {
-            currentTab.renderContent(graphics);
+        // TODO: Рендер рун у вибраній вкладці
+    }
+
+    public void openTabEditor(boolean isNew) {
+        inEditMode = true;
+        if (isNew) {
+            editingTab = new RuneTab("Нова вкладка");
+        } else {
+            editingTab = runeTabs.get(selectedTab);
+        }
+        // TODO: GUI для редагування
+    }
+
+    public void saveEditingTab() {
+        if (editingTab != null) {
+            if (!runeTabs.contains(editingTab)) {
+                RuneTabManager.addTab(editingTab);
+                runeTabs.add(editingTab);
+            } else {
+                RuneTabManager.updateTab(editingTab);
+            }
+        }
+        inEditMode = false;
+    }
+
+    public void deleteCurrentTab() {
+        if (selectedTab >= 0 && selectedTab < runeTabs.size()) {
+            RuneTab tab = runeTabs.get(selectedTab);
+            RuneTabManager.removeTab(tab);
+            runeTabs.remove(tab);
+            selectedTab = Math.max(0, selectedTab - 1);
         }
     }
 
-    public void selectTab(RuneTab tab) {
-        currentTab = tab;
+    public void tick() {
+        // Якщо потрібен оновлюваний інтерфейс
     }
-
-    // Методи для додавання нових вкладок, видалення (окрім "Всі") і т.д.
 }
