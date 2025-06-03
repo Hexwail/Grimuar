b6a899302412c6554047a7e7abb8ae85948fdf3e
Hexwail <ivankuzmichov2008@gmail.com>
2025-06-03 13:48:34 +0300

    Create RunePanel.java

---
+++ src/main/java/com/hexwail/grimuar/client/gui/editor/RunePanel.java
@@ -0,0 +1,85 @@
+package com.hexwail.grimuar.client.gui.editor;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Widget;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RunePanel extends Widget {
+
+    private static final ResourceLocation TEXTURE = new ResourceLocation("grimuar", "textures/gui/rune_panel.png");
+    private final Minecraft mc;
+    private final int x, y, width, height;
+
+    private final List<Rune> runes = new ArrayList<>();
+    private int scrollOffset = 0;
+
+    public RunePanel(int x, int y, int width, int height) {
+        super(x, y, width, height, Component.literal("Rune Panel"));
+        this.mc = Minecraft.getInstance();
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        // TODO: завантажити руни для показу
+    }
+
+    @Override
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        graphics.blit(TEXTURE, x, y, 0, 0, width, height);
+
+        int runeSize = 20;
+        int padding = 4;
+        int startX = x + padding;
+        int startY = y + padding;
+
+        int index = 0;
+        for (Rune rune : runes) {
+            int drawX = startX + (index % 4) * (runeSize + padding);
+            int drawY = startY + (index / 4) * (runeSize + padding) - scrollOffset;
+
+            if (drawY + runeSize >= y && drawY <= y + height) {
+                graphics.blit(rune.getTexture(), drawX, drawY, 0, 0, runeSize, runeSize);
+            }
+            index++;
+        }
+    }
+
+    // Додати методи для додавання/видалення рун, прокрутки, вибору тощо
+
+    public void addRune(Rune rune) {
+        runes.add(rune);
+    }
+
+    public void removeRune(Rune rune) {
+        runes.remove(rune);
+    }
+
+    // TODO: обробка кліків, виділення, прокрутки
+
+    // Вкладений клас або окремий клас Rune (можна винести в окремий файл)
+    public static class Rune {
+        private final ResourceLocation texture;
+        private final String name;
+
+        public Rune(ResourceLocation texture, String name) {
+            this.texture = texture;
+            this.name = name;
+        }
+
+        public ResourceLocation getTexture() {
+            return texture;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
