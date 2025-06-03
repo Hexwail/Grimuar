c113f9201b45a780af45806f5baa2a5e6bcfe308
Hexwail <ivankuzmichov2008@gmail.com>
2025-06-03 13:52:04 +0300

    Update SpellEditorScreen.java

--- src/main/java/com/hexwail/grimuar/client/gui/editor/SpellEditorScreen.java
+++ src/main/java/com/hexwail/grimuar/client/gui/editor/SpellEditorScreen.java
@@ -8,6 +8,7 @@ import net.minecraft.network.chat.Component;
 public class SpellEditorScreen extends Screen {
 
     private SpellCameraController camera;
+    private RunePanel runePanel;
 
     public SpellEditorScreen() {
         super(Component.literal("Grimuar Spell Editor"));
@@ -16,19 +17,37 @@ public class SpellEditorScreen extends Screen {
     @Override
     protected void init() {
         this.camera = new SpellCameraController(minecraft);
-        // TODO: ініціалізація вкладок, рун, збережених заклять
+
+        int panelWidth = 100;
+        int panelHeight = 80;
+        int panelX = this.width - panelWidth - 10;  // правий край з відступом 10
+        int panelY = this.height - panelHeight - 10; // нижній край з відступом 10
+
+        this.runePanel = new RunePanel(panelX, panelY, panelWidth, panelHeight);
+
+        // Приклад: додамо кілька "рунів"
+        runePanel.addRune(new RunePanel.Rune(new net.minecraft.resources.ResourceLocation("grimuar", "textures/runes/rune_fire.png"), "Fire Rune"));
+        runePanel.addRune(new RunePanel.Rune(new net.minecraft.resources.ResourceLocation("grimuar", "textures/runes/rune_water.png"), "Water Rune"));
     }
 
     @Override
     public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
         renderBackground(graphics);
         camera.renderCameraView(graphics, partialTicks);
-        // TODO: відображення панелей
+        runePanel.render(graphics, mouseX, mouseY, partialTicks);
         super.render(graphics, mouseX, mouseY, partialTicks);
     }
 
     @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (runePanel.mouseClicked(mouseX, mouseY, button)) {
+            return true;
+        }
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
     public boolean mouseDragged(double mouseX, double mouseY, int button, double dx, double dy) {
-        return camera.handleMouseDrag(dx, dy);
+        return camera.handleMouseDrag(dx, dy) || super.mouseDragged(mouseX, mouseY, button, dx, dy);
     }
 }
